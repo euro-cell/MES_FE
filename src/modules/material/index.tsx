70e1509ac79bdcbd29b0eb8f1ed5c5aa5cc05eb9
go-tiger <dev.gotiger@gmail.com>
2025-10-29 16:41:44 +0900

    feat: 자재관리 - 원부자재(전체) 페이지

--- src/modules/material/index.tsx
+++ src/modules/material/index.tsx
@@ -1,7 +1,19 @@
+import { useState } from 'react';
+import '../../styles/material/material.css';
+import MaterialNav from './MaterialNav';
+import RawMaterialList from './rawMaterial/RawMaterialList';
+
 export default function MaterialPage() {
+  const [activeTab, setActiveTab] = useState<'raw' | 'cell'>('raw');
+
   return (
-    <div>
-      <h2>자재관리</h2>
+    <div className='material-page'>
+      <MaterialNav activeTab={activeTab} setActiveTab={setActiveTab} />
+
+      <div className='material-content'>
+        {activeTab === 'raw' && <RawMaterialList />}
+        {/* {activeTab === 'cell' && <CellMaterialList />} */}
+      </div>
     </div>
   );
 }
