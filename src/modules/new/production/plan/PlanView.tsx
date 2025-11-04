40379fe0929ab690dd1d1fb03f42ae63cc59e171
go-tiger <dev.gotiger@gmail.com>
2025-11-04 16:50:01 +0900

    fix: 콘솔로그 제거

--- src/modules/new/production/plan/PlanView.tsx
+++ src/modules/new/production/plan/PlanView.tsx
@@ -71,8 +71,6 @@ export default function PlanView() {
           weekHeaders: data.weekHeaders.flat(),
         };
       }
-
-      console.log('📊 Plan Data:', data);
       setPlanData(data);
     });
   }, [productionId]);
