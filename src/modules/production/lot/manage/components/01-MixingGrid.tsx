b9a0a6d687965be18c94253980b8355eb6fc18eb
go-tiger <dev.gotiger@gmail.com>
2025-12-19 10:30:57 +0900

    fix: lot관리 - 믹싱 스타일 수정

--- src/modules/production/lot/manage/components/01-MixingGrid.tsx
+++ src/modules/production/lot/manage/components/01-MixingGrid.tsx
@@ -35,45 +35,43 @@ export default function MixingGrid({ data }: MixingGridProps) {
     <div className={styles.mixingGrid}>
       <table className={styles.mixingTable}>
         <thead>
-          {/* 그룹 헤더 */}
+          {/* 대분류 헤더 */}
           <tr>
-            <th ref={firstColRef} className={`${styles.groupBasic} ${styles.stickyCol} ${styles.stickyFirst}`}></th>
-            <th className={`${styles.groupBasic} ${styles.stickyCol} ${styles.stickySecond}`} style={{ left: secondColLeft }}></th>
+            <th ref={firstColRef} rowSpan={3} className={`${styles.headerBasicCol} ${styles.stickyCol} ${styles.stickyFirst}`}>Mixing Date</th>
+            <th rowSpan={3} className={`${styles.headerBasicCol} ${styles.stickyCol} ${styles.stickySecond}`} style={{ left: secondColLeft }}>Lot</th>
             <th colSpan={2} className={styles.groupMixing}>at Mixing</th>
             <th colSpan={1} className={styles.groupInput}>INPUT</th>
             <th colSpan={3} className={styles.groupViscosity}>Viscosity</th>
             <th colSpan={4} className={styles.groupElectrode}>Electrode Spec.</th>
             <th colSpan={4} className={styles.groupBinder}>Binder</th>
           </tr>
-          {/* 컬럼 헤더 (중) */}
+          {/* 중분류 헤더 */}
           <tr>
-            <th className={`${styles.stickyCol} ${styles.stickyFirst}`}>Mixing Date</th>
-            <th className={`${styles.stickyCol} ${styles.stickySecond}`} style={{ left: secondColLeft }}>Lot</th>
+            {/* Mixing Date, Lot은 대분류에서 rowSpan={3} */}
             <th>Temp</th>
-            <th>Humidity</th>
-            <th>Active Material</th>
+            <th className={styles.groupMixingEnd}>Humidity</th>
+            <th className={styles.groupInputEnd}>Active Material</th>
             <th>After Mixing</th>
             <th>탈포 후</th>
-            <th>안정화 후</th>
+            <th className={styles.groupViscosityEnd}>안정화 후</th>
             <th>Grind Gage</th>
-            <th colSpan={3}>solid content</th>
+            <th colSpan={3} className={styles.groupElectrodeEnd}>solid content</th>
             <th>viscosity</th>
             <th colSpan={3}>solid content</th>
           </tr>
           {/* 단위 헤더 */}
           <tr>
-            <th className={`${styles.stickyCol} ${styles.stickyFirst}`}></th>
-            <th className={`${styles.stickyCol} ${styles.stickySecond}`} style={{ left: secondColLeft }}></th>
+            {/* Mixing Date, Lot은 대분류에서 rowSpan={3} */}
             <th>°C</th>
-            <th>%</th>
-            <th>kg</th>
+            <th className={styles.groupMixingEnd}>%</th>
+            <th className={styles.groupInputEnd}>kg</th>
             <th>CPS</th>
             <th>CPS</th>
-            <th>CPS</th>
+            <th className={styles.groupViscosityEnd}>CPS</th>
             <th>μm</th>
             <th>%</th>
             <th>%</th>
-            <th>%</th>
+            <th className={styles.groupElectrodeEnd}>%</th>
             <th>CPS</th>
             <th>%</th>
             <th>%</th>
@@ -85,18 +83,18 @@ export default function MixingGrid({ data }: MixingGridProps) {
             const { temp, humi } = parseTempHumi(row.slurry.tempHumi);
             return (
               <tr key={row.id}>
-                <td className={`${styles.stickyCol} ${styles.stickyFirst}`}>{row.processDate}</td>
-                <td className={`${styles.lotNumber} ${styles.stickyCol} ${styles.stickySecond}`} style={{ left: secondColLeft }}>{row.lot}</td>
+                <td className={`${styles.stickyCol} ${styles.stickyFirst} ${styles.groupBasic}`}>{row.processDate}</td>
+                <td className={`${styles.lotNumber} ${styles.stickyCol} ${styles.stickySecond} ${styles.groupBasic}`} style={{ left: secondColLeft }}>{row.lot}</td>
                 <td>{temp}</td>
-                <td>{humi}</td>
-                <td>{row.slurry.activeMaterialInput}</td>
+                <td className={styles.groupMixingEnd}>{humi}</td>
+                <td className={styles.groupInputEnd}>{row.slurry.activeMaterialInput}</td>
                 <td>{row.slurry.viscosityAfterMixing} cp</td>
                 <td>{row.slurry.viscosityAfterDefoaming} cp</td>
-                <td>{row.slurry.viscosityAfterStabilization} cp</td>
+                <td className={styles.groupViscosityEnd}>{row.slurry.viscosityAfterStabilization} cp</td>
                 <td>{row.slurry.grindGage}</td>
                 <td>{row.slurry.solidContent1}%</td>
                 <td>{row.slurry.solidContent2}%</td>
-                <td>{row.slurry.solidContent3}%</td>
+                <td className={styles.groupElectrodeEnd}>{row.slurry.solidContent3}%</td>
                 <td>{row.binder.viscosity !== null ? `${row.binder.viscosity} cp` : '-'}</td>
                 <td>{row.binder.solidContent1}%</td>
                 <td>{row.binder.solidContent2}%</td>
