e1bf656defff90aa505decf9fe6ff49ddf375bd0
go-tiger <dev.gotiger@gmail.com>
2026-03-18 11:33:26 +0900

    feat: LQC 조립 Sealing R 관리도 차트 추가

--- src/modules/quality/lqc/components/assembly/SealingDashboard.tsx
+++ src/modules/quality/lqc/components/assembly/SealingDashboard.tsx
@@ -2,6 +2,7 @@ import { useState } from 'react';
 import SealingTopTable from './SealingTopTable';
 import SealingMeasurementTable from './SealingMeasurementTable';
 import SealingXbarChart from './SealingXbarChart';
+import SealingRChart from './SealingRChart';
 import ControlChartConstantsTable from '../common/ControlChartConstantsTable';
 import type { SealingMeasurementRow } from './SealingMeasurementTable';
 import styles from '../../../../../styles/quality/lqc/LQCTable.module.css';
@@ -34,6 +35,18 @@ export default function SealingDashboard({ projectId }: SealingDashboardProps) {
         yMin={720}
         yMax={780}
         yStep={10}
+        data={topRows}
+      />
+      <SealingRChart
+        title="R 관리도 - Side Sealing"
+        yMax={30}
+        yStep={5}
+        data={sideRows}
+      />
+      <SealingRChart
+        title="R 관리도 - Top Sealing"
+        yMax={25}
+        yStep={5}
         data={topRows}
       />
       <ControlChartConstantsTable currentN={6} />
