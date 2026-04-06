8c8e0dd21fec39320aa73dfef209e6f1422bc12f
고랑 <inter_tiger_@naver.com>
2026-04-06 17:09:59 +0900

    chore: PM2 ecosystem 설정 파일 추가

---
+++ ecosystem.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'eurocell-mes',
+      script: 'C:\\Windows\\System32\\cmd.exe',
+      args: '/c npm run dev',
+      cwd: 'C:\\Users\\user\\Desktop\\eurocell-mes-fe',
+      interpreter: 'none',
+    },
+  ],
+};
